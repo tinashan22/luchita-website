b7889550080d6f25185dc67c88de31e831daecd5
Tina Shan <tinashan22@gmail.com>
2024-01-13 19:51:31 -0800

    refactored unused code

--- src/app/checkout/checkout.tsx
+++ src/app/checkout/checkout.tsx
@@ -6,12 +6,6 @@ import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import getStripe from "@/utils/getStripe";
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-// const stripePromise = loadStripe(
-//   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-// );
-
 const stripePromise = getStripe();
 const projectUrl = process.env.NEXTAUTH_URL;
 
@@ -46,12 +40,6 @@ export default function CheckoutPage() {
 
       const res = await checkoutResponse.json();
       console.log(res, "session");
-
-      //   const stripeError = await stripe.redirectToCheckout({ session.sessionId });
-
-      //   if (stripeError) {
-      //     console.error(stripeError);
-      //   }
     } catch (error) {
       console.error(error);
     }
@@ -63,33 +51,5 @@ export default function CheckoutPage() {
         Checkout
       </button>
     </section>
-
-    //   <style jsx>
-    //     {`
-    //       section {
-    //         background: #ffffff;
-    //         display: flex;
-    //         flex-direction: column;
-    //         width: 400px;
-    //         height: 112px;
-    //         border-radius: 6px;
-    //         justify-content: space-between;
-    //       }
-    //       button {
-    //         height: 36px;
-    //         background: #556cd6;
-    //         border-radius: 4px;
-    //         color: white;
-    //         border: 0;
-    //         font-weight: 600;
-    //         cursor: pointer;
-    //         transition: all 0.2s ease;
-    //         box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
-    //       }
-    //       button:hover {
-    //         opacity: 0.8;
-    //       }
-    //     `}
-    //   </style>
   );
 }
--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -3,20 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
+// Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDacNTzALuLvLjvB2wz03jklqZvrTbmNAQ",
-//   authDomain: "lucha-luchita.firebaseapp.com",
-//   projectId: "lucha-luchita",
-//   storageBucket: "lucha-luchita.appspot.com",
-//   messagingSenderId: "297292691045",
-//   appId: "1:297292691045:web:0c2576deb3de65e6d9b88c",
-//   measurementId: "G-Z1Q747CF82",
-// };
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -33,22 +21,3 @@ export const firebase = initializeApp(firebaseConfig);
 export const auth = getAuth(firebase);
 
 //export const analytics = getAnalytics(firebase);
-
-//======
-// const firebaseConfig = JSON.parse(process?.env?.FIREBASE_CONFIG ?? "{}");
-
-// let analytics;
-// let firestore;
-// if (firebaseConfig?.projectId) {
-//   // Initialize Firebase
-//   const app = initializeApp(firebaseConfig);
-
-//   if (app.name && typeof window !== "undefined") {
-//     analytics = getAnalytics(app);
-//   }
-
-//   // Access Firebase services using shorthand notation
-//   const firestore = getFirestore();
-// }
-
-// export { analytics, firestore };
